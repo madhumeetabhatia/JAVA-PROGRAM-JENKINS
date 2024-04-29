9197b807e3f00af147ff069f62a60e2abfb69a64
madhumita bhatia <madhumeetabhatia@gmail.com>
2024-04-29 21:36:55 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is java program");
 		System.out.println(new Date());
+		System.out.println("Hello");     
 
 	}
 
