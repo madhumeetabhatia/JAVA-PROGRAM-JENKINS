82627e425496451457eaeec7acd74f472d91146a
madhumita bhatia <madhumeetabhatia@gmail.com>
2024-05-01 20:05:59 +0530

    First commit

--- src/jenkinTest.java
+++ src/jenkinTest.java
@@ -5,12 +5,6 @@ public class jenkinTest {
 	public static void main(String[] args) {
 		System.out.println("This is java program");
 		System.out.println(new Date());
-		System.out.println("Hello");     
-		System.out.println("Hello");     
-		System.out.println("second commit"); 
-		System.out.println("Third commit"); 
-		System.out.println("Forth commit"); 
-		System.out.println("fifth commit"); 
-	}
+		
 
-}
+}}
